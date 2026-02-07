cb9f02b7fa7f8ab99165c4e155bb7c186d98b6e6
KING <dbswjddn6@gmail.com>
2026-02-07 17:51:29 +0900

    Feat: Add Entites and Enums

--- src/main/java/whoreads/backend/domain/dna/enums/TrackCode.java
+++ src/main/java/whoreads/backend/domain/dna/enums/TrackCode.java
@@ -1,6 +1,12 @@
 package whoreads.backend.domain.dna.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum TrackCode {
+
     COMFORT("마음 정리/위로"),
     HABIT("실행력/습관"),
     CAREER("커리어/시야 넓히기"),
@@ -8,8 +14,4 @@ public enum TrackCode {
     FOCUS("재미/몰입");
 
     private final String description;
-
-    TrackCode(String description) {
-        this.description = description;
-    }
 }
