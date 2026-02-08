c3e3cdcdd67b7f375661daf3e83a138efc40c622
yujin <yujin09219@naver.com>
2026-02-09 01:47:55 +0900

    refactor: 스케줄러 로직 개선, 토큰 서비스 스케줄러에서 스케줄러 파일로 분리

--- src/main/java/whoreads/backend/domain/notification/service/NotificationScheduler.java
+++ src/main/java/whoreads/backend/domain/notification/service/NotificationScheduler.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import whoreads.backend.domain.member.repository.MemberRepository;
 import whoreads.backend.domain.notification.dto.MemberTokenDTO;
 import whoreads.backend.domain.notification.event.NotificationEvent;
 import whoreads.backend.domain.notification.repository.NotificationHistoryRepository;
@@ -25,6 +27,7 @@ public class NotificationScheduler {
     private final NotificationSettingRepository notificationSettingRepository;
     private final ApplicationEventPublisher applicationEventPublisher;
     private final NotificationHistoryRepository notificationHistoryRepository;
+    private final MemberRepository memberRepository;
 
     @Scheduled(cron = "0 * * * * *",zone = "Asia/Seoul")
     public void checkRoutineNotifications() {
@@ -47,9 +50,20 @@ public class NotificationScheduler {
             throw new CustomException(ErrorCode.FCM_SEND_FAILED);
         }
     }
+    @Transactional
     @Scheduled(cron = "0 0 0 * * *",zone = "Asia/Seoul") // 매일 자정 실행
     public void deleteOldNotifications() {
         LocalDateTime cutoff = LocalDateTime.now().minusDays(30);
         notificationHistoryRepository.deleteByCreatedAtBefore(cutoff);
+    }
+    /*
+     * 매일 2시 30일 지난 토큰 정리
+     * */
+    @Scheduled(cron = "0 0 2 * * *",zone = "Asia/Seoul")
+    @Transactional
+    public void deleteInactiveUsers() {
+        LocalDateTime threshold = LocalDateTime.now().minusDays(30);
+        memberRepository.clearExpiredTokens(threshold);
+        log.info("미접속자 토큰 제거 완료");
     }
 }
